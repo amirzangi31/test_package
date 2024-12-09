df82b9fd195a515a23ab9e03720275bd0d78cd25
amirmohammad zangiabadi <zangiabadi1378888@gmail.com>
2024-12-09 14:37:22 +0330

    test

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -9,14 +9,14 @@ const buttonStyles = cva(
                   variant: {
                         primary: ["bg-red-900", "rounded-full", "text-white", "hover:bg-white", "hover:text-red-900", "hover:border", "hover:border-red-900"],
                         outlined: ["border", "border-red-900", "bg-transparent", "rounded-full", "hover:bg-red-900", "hover:text-white", "text-red-900"],
-                        
+                        secondary: ["bg-green-700", "rounded-full", "text-white", "hover:bg-white", "hover:text-green-700", "hover:border", "hover:border-green-700"]
                   },
                   size: {
                         default: ["", "w-20", "h-auto", "p-2"],
                         button: ["", "h-10", "w-32"],
                         icon: ["", "w-20", "h-20", "p-2.5"],
                   },
-                  
+
             },
 
             defaultVariants: {
@@ -26,7 +26,7 @@ const buttonStyles = cva(
       }
 );
 
-type buttonType = VariantProps<typeof buttonStyles> & ComponentProps<"button"> & {
+export type buttonType = VariantProps<typeof buttonStyles> & ComponentProps<"button"> & {
       classname?: string,
       handler?: () => void
       isLoading?: boolean
@@ -35,7 +35,7 @@ type buttonType = VariantProps<typeof buttonStyles> & ComponentProps<"button"> &
 };
 
 const Button = ({ variant, size, children, handler, isDisabled, isLoading, className, ...props }: buttonType) => {
-      
+
       return (
             <button
                   {...props}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import Button from './components/button/Button'
+import Button from "./components/button/Button";
 
-export { Button }
+export { Button };
+export type { buttonType } from "./components/button/Button";
